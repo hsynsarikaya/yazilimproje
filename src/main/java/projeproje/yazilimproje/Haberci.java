f850ad3e56ed87e877e3d6a51aa92872bd22c89e
hsynsarikaya <gotrimes@gmail.com>
2018-02-01 18:38:19 +0300

    Update Haberci.java

--- src/main/java/projeproje/yazilimproje/Haberci.java
+++ src/main/java/projeproje/yazilimproje/Haberci.java
@@ -1,19 +1,33 @@
+/*
+ * @author HuseyinSarikaya
+ * 
+ */
 package projeproje.yazilimproje;
 
-
+/*
+ * @param count, id 
+ * 
+ * 
+ */
 public class Haberci {
     public static int COUNT = 0;
-    private int id;
+    private int id; 
     private String haber;
-    
-    public Haberci(String haber) {
+    private String tarih;
+    public Haberci(String haber,String tarih) {
         this.id = COUNT;
         this.haber = haber;
-       
+        this.tarih = tarih;
+    /*
+     * @param getHaber() 
+     * 
+     */
     }
     public String getHaber() {
         return haber;
     }
-  
+    public String getTarih() {
+        return tarih;
+    }
     
 }
